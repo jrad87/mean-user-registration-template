dd2f3383a58c34491422722194942f047bd1e53d
Jared Brown <jared.brown792@gmail.com>
2017-11-16 22:48:13 -0800

    adding error-handler.js and others

---
+++ src/server/utils/error-handler.js
@@ -0,0 +1,11 @@
+module.exports = function(error){
+	let errors = [];
+	if(error.errors){
+		errors = Object.keys(error.errors).map(key => error.errors[key].message);
+	} else if (typeof error === 'string') {
+		errors.push(error);
+	} else {
+		errors.push(error.message);
+	}
+	this.status(422).json(errors);
+}
